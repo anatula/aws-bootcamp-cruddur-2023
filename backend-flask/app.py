cecf96c543c04a803ab2083ef66364b40ea26aa1
Ana Laura T <analaura.tula@gmail.com>
2023-04-30 21:41:56 +0000

    fix error 'incomplete placeholder' and hardcoded my user handle

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -197,7 +197,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'anatula'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -8,4 +8,4 @@ SELECT
 FROM public.activities
 INNER JOIN public.users ON users.uuid = activities.user_uuid 
 WHERE 
-  activities.uuid = %(uuid)
+  activities.uuid = %(uuid)s
